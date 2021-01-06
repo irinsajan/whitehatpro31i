12f01f520e17ea31b0a7f8821efbd83c7763ba46
irinsajan <69659076+irinsajan@users.noreply.github.com>
2021-01-06 16:58:01 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -15,21 +15,32 @@ function setup() {
   world = engine.world;
   ground = new Ground(240,695,480,10);
   
-}
-
-function draw() {
-  background("black");  
-  Engine.update(engine);
-  for (let k = 0; k <=width; k=k+80) {
+   for (let k = 0; k <=width; k=k+80) {
     division.push(new Division(k, height-divisionHeight/2, 10, divisionHeight))
   }
-  for (let j = 40; j <= width; j=j50) {
+  for (let j = 40; j <= width; j=j+50) {
     plinko.push( new Plinko(j,75));
   }
   for (let j = 15; j <=width-10; j=j+50) {
     plinko.push(new Plinko(j,175));
     
   }
+  
+  
+}
+
+function draw() {
+  background("black");  
+  Engine.update(engine);
+ 
+  for(var k=0; k<division.length; k++){
+    division[k].display();
+  }
+
+  for(var k=0; k<plinko.length; k++){
+    plinko[k].display();
+  }
+  
   //division.display();
   ground.display();
 }
